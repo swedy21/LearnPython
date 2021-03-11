70c31af3152e28854ea908c671218d4a1619de7b
swedy21 <lazar.istvan@gmail.com>
2021-03-11 22:18:36 +0100

    First Git commit. Add Basic/FunGUI.py

---
+++ Basic/FunGUI.py
@@ -0,0 +1,23 @@
+picture = [
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0]
+]
+
+# print(len(picture))
+
+item = 0
+while item < len(picture):
+    my_pic = ''
+    for i in picture[item]:
+        if i == 0:
+            my_pic += ' '
+        elif i == 1:
+            my_pic += '*'
+    print('\n', my_pic)
+    item += 1
+
+    print
